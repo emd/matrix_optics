32642a4f023322b18be08e433096ecfde38a18cb
emd <emd@mit.edu>
2018-07-13 11:41:02 +0200

    Enforce symbol properties for symbolic Gaussian beams

---
+++ matrix_optics/symbolic/gaussian_beam.py
@@ -0,0 +1,175 @@
+import sympy
+from sympy.core.numbers import pi, I, oo
+
+
+# class GaussianBeam(object):
+#     def __init__(self, q, w=None, R=None,
+#                  wavelength=sympy.symbols('lambda0', positive=True)):
+#         '''Create an instance of symbolic `GaussianBeam` class.
+# 
+#         Input parameters:
+#         -----------------
+#         q - :py:class:`Symbol <sympy.core.symbol.Symbol>` instance, or None
+#             Complex beam parameter. If `None`, use `w` and `R` to
+#             determine the corresponding value of `q`.
+#             [q] = [wavelength]
+# 
+#         w - :py:class:`Symbol <sympy.core.symbol.Symbol>` instance
+#             1/e E radius of Gaussian beam. Only used if `q` is `None`.
+#             Note that `w` must be explicitly *positive*.
+#             [w] = [wavelength]
+# 
+#         R - :py:class:`Symbol <sympy.core.symbol.Symbol>` instance
+#             Radius of curvature of Gaussian beam. Only used if `q` is `None`.
+#             Note that `R` must be explicitly *real* and that a value of
+#             infinity corresponds to a beam waist.
+#             [R] = [wavelength]
+# 
+#         wavelength - :py:class:`Symbol <sympy.core.symbol.Symbol>` instance
+#             Wavelength of Gaussian beam. Note that `wavelength` must be
+#             explicitly *positive*.
+#             [wavelength] = arbitrary units
+# 
+#         '''
+#         # Enforce that wavelength is always positive.
+#         if wavelength.is_positive:
+#             self.wavelength = wavelength
+#         else:
+#             raise ValueError('`wavelength` must be explicitly *positive*.')
+# 
+#         if q is None:
+#             if (w is not None) and (R is not None):
+#                 # Enforce that 1/e E radius `w` is always positive and
+#                 # that the radius of curvature `R` is always real prior
+#                 # to performing any computations.
+#                 assert w.is_positive, '`w` must be explicitly *positive*.'
+#                 assert R.is_real, '`R` must be explicitly *real*.'
+# 
+#                 qinv = (1 / R) - (I * self.wavelength / (pi * (w ** 2)))
+#                 self.q = sympy.simplify(1 / qinv)
+#             else:
+#                 raise ValueError(
+#                     'Both `w` & `R` must be specified if `q` is None')
+#             pass
+#         else:
+#             self.q = q
+# 
+#     @property
+#     def R(self):
+#         'Get radius of curvature.'
+#         Rinv = sympy.re(1 / self.q)
+# 
+#         if Rinv == 0:
+#             return oo
+#         else:
+#             return sympy.simplify(1 / Rinv)
+# 
+#     @property
+#     def w(self):
+#         'Get 1/e E radius.'
+#         qinv = 1 / self.q
+#         return sympy.sqrt(-self.wavelength / (pi * sympy.im(qinv)))
+
+
+def w(z, w0, zR):
+    '''Gaussian beam 1/e E radius.
+
+    Parameters:
+    -----------
+    z - :py:class:`Symbol <sympy.core.symbol.Symbol>` instance
+        The axial distance from the beam waist.
+        [z] = arbitrary units
+
+    w0 - :py:class:`Symbol <sympy.core.symbol.Symbol>` instance
+        1/e E radius of beam waist.
+        [w0] = [z]
+
+    zR - :py:class:`Symbol <sympy.core.symbol.Symbol>` instance
+        Rayleigh range of beam.
+        [zR] = [z]
+
+    Returns:
+    --------
+    w - :py:class:`Symbol <sympy.core.symbol.Symbol>` instance
+        The beam 1/e E radius as a function of the axial distance `z`
+        from the beam waist.
+        [w] = [z]
+
+    '''
+    return w0 * sympy.sqrt(1 + ((z / zR) ** 2))
+
+
+def R(z, zR):
+    '''Gaussian beam radius of curvature.
+
+    Parameters:
+    -----------
+    z - :py:class:`Symbol <sympy.core.symbol.Symbol>` instance
+        The axial distance from the beam waist.
+        [z] = arbitrary units
+
+    zR - :py:class:`Symbol <sympy.core.symbol.Symbol>` instance
+        Rayleigh range of beam.
+        [zR] = [z]
+
+    Returns:
+    --------
+    R - :py:class:`Symbol <sympy.core.symbol.Symbol>` instance
+        The beam radius of curvature as a function of the axial distance `z`
+        from the beam waist.
+        [R] = [z]
+
+    '''
+    # Waist needs special treatment, as sympy does not correctly evaluate
+    # the general expression.
+    if z == 0:
+        return oo
+    else:
+        return sympy.simplify(z * (1 + ((zR / z) ** 2)))
+
+
+def _check_symbol(sym, var):
+    '''Check that symbol `sym` has required properties for variable `var`.
+
+    Input parameters:
+    -----------------
+    sym - :py:class:`Symbol <sympy.core.symbol.Symbol>` instance
+        The symbol to be used for representation of variable `var`.
+
+    var - string
+        The variable to be represented by symbol `sym`.
+        Accepted variables are
+
+                ['z', 'R', 'w', 'w0', 'zR']
+
+        otherwise a ValueError is raised.
+
+    Returns:
+    --------
+    None if symbol `sym` has required properties for variable `var`;
+        otherwise an exception is raised.
+
+    '''
+    # Ensure `sym` is an instance of sympy `Symbol` class
+    if not isinstance(sym, sympy.Symbol):
+        raise TypeError('`sym` must be an instance of `sympy.Symbol` class.')
+
+    if var == 'z':
+        # Axial distance `z` must be explicitly real
+        assert sym.is_real, '`%s` must be explicitly *real*.' % sym
+    elif var == 'R':
+        # Radius of curvature `R` must be explicitly real
+        assert sym.is_real, '`%s` must be explicitly *real*.' % sym
+    elif var == 'w':
+        # 1/e E radius `w` must be explicitly positive
+        assert sym.is_positive, '`%s` must be explicitly *positive*.' % sym
+    elif var == 'w0':
+        # 1/e E beam-waist radius `w0` must be explicitly positive
+        assert sym.is_positive, '`%s` must be explicitly *positive*.' % sym
+    elif var == 'zR':
+        # Rayleigh range `zR` must be explicitly positive
+        assert sym.is_positive, '`%s` must be explicitly *positive*.' % sym
+    else:
+        raise ValueError("'%s' *not* a recognized variable." % var)
+
+    return
---
+++ tests/symbolic_tests/gaussian_beam_tests.py
@@ -0,0 +1,118 @@
+from nose import tools
+import sympy
+from sympy.core.numbers import pi, I, oo
+
+from matrix_optics.symbolic.gaussian_beam import (
+    w, R, _check_symbol)
+
+
+def test__check_symbol():
+    # Only sympy.Symbol instances are accepted
+    tools.assert_raises(
+        TypeError,
+        _check_symbol,
+        1, 'z')
+
+    # Axial distances must be explicitly real
+    z = sympy.symbols('z')
+    tools.assert_raises(
+        AssertionError,
+        _check_symbol,
+        z, 'z')
+
+    z = sympy.symbols('z', real=True)
+    _check_symbol(z, 'z')
+
+    # Radii of curvature must be explicitly real
+    R = sympy.symbols('R')
+    tools.assert_raises(
+        AssertionError,
+        _check_symbol,
+        R, 'R')
+
+    R = sympy.symbols('R', real=True)
+    _check_symbol(R, 'R')
+
+    # 1/e E radii must be explicitly positive
+    w = sympy.symbols('w')
+    tools.assert_raises(
+        AssertionError,
+        _check_symbol,
+        w, 'w')
+
+    w = sympy.symbols('w', positive=True)
+    _check_symbol(w, 'w')
+
+    # 1/e E beam-waist radii must be explicitly positive
+    w0 = sympy.symbols('w0')
+    tools.assert_raises(
+        AssertionError,
+        _check_symbol,
+        w0, 'w0')
+
+    w0 = sympy.symbols('w0', positive=True)
+    _check_symbol(w0, 'w0')
+
+    # Rayleigh ranges must be explicitly positive
+    zR = sympy.symbols('zR')
+    tools.assert_raises(
+        AssertionError,
+        _check_symbol,
+        zR, 'zR')
+
+    zR = sympy.symbols('zR', positive=True)
+    _check_symbol(zR, 'zR')
+
+    # Non-recognized variable `a` raises ValueError
+    tools.assert_raises(
+        ValueError,
+        _check_symbol,
+        zR, 'a')
+
+    return
+
+
+def test_w_and_R():
+    z, w0, zR = sympy.symbols('z, w0, zR', real=True)
+
+    # Straightforward algebra with R(z) and w(z) shows that
+    # the below equality is true. (Further, a few additional
+    # lines of algebra show that q = z + (i * zR)).
+    tools.assert_true(sympy.Equality(
+        sympy.simplify(R(z, zR) / (w(z, w0, zR) ** 2)),  # simplify required!
+        (zR ** 2) / (z * (w0 ** 2))))
+
+    tools.assert_true(sympy.Equality(
+        R(0, zR),
+        oo))
+
+    return
+
+
+# def test_GaussianBeam__init__():
+#     z = sympy.symbols('z', real=True)
+#     w0, zR = sympy.symbols('w0, zR', positive=True)
+#     lambda0 = pi * (w0 ** 2) / zR
+# 
+#     # Test for arbitrary `z`
+#     g = GaussianBeam(
+#         None,
+#         w=w(z, w0, zR),
+#         R=R(z, zR),
+#         wavelength=lambda0)
+# 
+#     tools.assert_true(sympy.Equality(
+#         sympy.factor(g.q, gaussian=True),
+#         z + (I * zR)))
+# 
+#     tools.assert_true(sympy.Equality(
+#         g.R,
+#         R(z, zR)))
+# 
+#     tools.assert_true(sympy.Equality(
+#         g.w,
+#         w(z, w0, zR)))
+# 
+#     # Test for arbitrary `z` = 0
+# 
+#     return
